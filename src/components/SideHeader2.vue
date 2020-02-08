405de717ea6a9c492ab67ff69770e41190276fb7
Kensaku33 <picow.zoe33@gamil.com>
2020-02-08 09:13:50 +0900

    aboutme

--- src/components/SideHeader2.vue
+++ src/components/SideHeader2.vue
@@ -1,82 +1,87 @@
 <template>
   <div id="side-header2">
-    <div id="vertical-box" v-bind:class='{active:isActive}'>
-      <a class="menu-trigger" href="#" v-on:click='isActive=!isActive' v-bind:class='{active:isActive}'>
-        <span></span>
-        <span></span>
-        <span></span>
-      </a>
-      <p class="vertical-text" v-bind:class='{active:isActive}'>ABOUT ME</p>
 
-      <div id="about" v-bind:class='{active:isActive}'>
-        <div id="abotor"></div>
+    <transition name="right" appear>
+      <div id="vertical-box" v-bind:class='{active:isActive}'>
+        <a class="menu-trigger" href="#" v-on:click='isActive=!isActive' v-bind:class='{active:isActive}'>
+          <span></span>
+          <span></span>
+          <span></span>
+        </a>
+        <p class="vertical-text" v-bind:class='{active:isActive}'>ABOUT ME</p>
 
-        <transition name="left">
-          <div class="profile-box" v-if="profile">
-            <h3 class="profile-title">profile</h3>
-            <p>ヤマゾエケンサク</p>
-            <br>
-            <p>徳島県出身</p><br>
-            <span class="profile-inner">
-              <p>スノーボードとか釣りとか<br>アウトドアなものが好き。</p>
-              <p>海外で旅していた時に<br>友人からもらった写真に感銘を受けて写真を始める。</p>
-            </span>
-            <span class="close" @click="profile=!profile">close</span>
+        <div id="about" v-bind:class='{active:isActive}'>
+          <div id="abotor">
+            <img class="abotor" src="https://lh3.googleusercontent.com/_9XoqeAV2iEsxtEm4ofIrAdTMwYHJHemxW13Y29R9oEqxWPV2G0tWuDVNCSzT05_tE-vl0fQf9RKRHbMK6kvmEc_tXfx2s7syV-DQ5aqJ1hVmCxSrpKaZz-i4o8IN_GAQ0CiZ8ezw-c=w2400" alt="本人写真">
           </div>
-        </transition>
 
-        <transition name="left">
-          <div class="works-box" v-if="works">
-            <h3 class="works-title">works</h3>
-            <p>写真撮影を中心にいろいろやってます</p>
-            <p>写真を扱った制作物が得意です</p>
-            <br>
-            <span class="works-inner">
-              <p>写真 / 動画撮影</p>
-              <p>写真 / 動画編集</p>
-              <p>広告、ポスター作成</p>
-            </span>
-            <span class="close" @click="works=!works">close</span>
-          </div>
-        </transition>
-
-        <transition name="left">
-          <div class="skills-box" v-if="skills">
-            <h3 class="skills-title">skills</h3>
-            <!-- <br> -->
-            <span class="skills-inner">
-              <p>ツール</p>
-              <p>illustrator / photoshop</p>
-              <p>premiere / lightroom</p>
-              <p>XD</p>
+          <transition name="left">
+            <div class="profile-box" v-if="profile">
+              <h3 class="profile-title">profile</h3>
+              <p>ヤマゾエケンサク</p>
               <br>
-              <p>エンジニアスキル</p>
-              <p>HTML / CSS</p>
-              <p>jQuery / vue.js</p>
-              <p>Ruby on Rails</p>
-              <p>Git / AWS</p>
-            </span>
-            <span class="close" @click="skills=!skills">close</span>
-          </div>
-        </transition>
+              <p>徳島県出身</p><br>
+              <span class="profile-inner">
+                <p>スノーボードとか釣りとか<br>アウトドアなものが好き。</p>
+                <p>海外で旅していた時に<br>友人からもらった写真に感銘を受けて写真を始める。</p>
+              </span>
+              <span class="close" @click="profile=!profile">close</span>
+            </div>
+          </transition>
 
-        <transition name="right">
-          <ul id="menu" v-if="!profile && !works && !skills">
-            <li class="list" id="profile" @click="profile=!profile">profile</li>
-            <li class="list" id="works" @click="works=!works">works</li>
-            <li class="list" id="skills" @click="skills=!skills">skills</li>
-            <!-- <li class="list" id="contact">contact</li> -->
-          </ul>
-        </transition>
+          <transition name="left">
+            <div class="works-box" v-if="works">
+              <h3 class="works-title">works</h3>
+              <p>写真撮影を中心にいろいろやってます</p>
+              <p>写真を扱った制作物が得意です</p>
+              <br>
+              <span class="works-inner">
+                <p>写真 / 動画撮影</p>
+                <p>写真 / 動画編集</p>
+                <p>広告、ポスター作成</p>
+              </span>
+              <span class="close" @click="works=!works">close</span>
+            </div>
+          </transition>
 
-          <ul id="social" v-bind:class='{active:isActive}'>
-            <li class="sns"><a :href="twitter"><i class="fab fa-twitter"></i></a></li>
-            <li class="sns"><a :href="facebook"><i class="fab fa-facebook"></i></a></li>
-            <li class="sns"><a :href="instagram"><i class="fab fa-instagram"></i></a></li>
-          </ul>
+          <transition name="left">
+            <div class="skills-box" v-if="skills">
+              <h3 class="skills-title">skills</h3>
+              <!-- <br> -->
+              <span class="skills-inner">
+                <p>ツール</p>
+                <p>illustrator / photoshop</p>
+                <p>premiere / lightroom</p>
+                <p>XD</p>
+                <br>
+                <p>エンジニアスキル</p>
+                <p>HTML / CSS</p>
+                <p>jQuery / vue.js</p>
+                <p>Ruby on Rails</p>
+                <p>Git / AWS</p>
+              </span>
+              <span class="close" @click="skills=!skills">close</span>
+            </div>
+          </transition>
+
+          <transition name="right">
+            <ul id="menu" v-if="!profile && !works && !skills">
+              <li class="list" id="profile" @click="profile=!profile">profile</li>
+              <li class="list" id="works" @click="works=!works">works</li>
+              <li class="list" id="skills" @click="skills=!skills">skills</li>
+              <!-- <li class="list" id="contact">contact</li> -->
+            </ul>
+          </transition>
+
+            <ul id="social" v-bind:class='{active:isActive}'>
+              <li class="sns"><a :href="twitter"><i class="fab fa-twitter"></i></a></li>
+              <li class="sns"><a :href="facebook"><i class="fab fa-facebook"></i></a></li>
+              <li class="sns"><a :href="instagram"><i class="fab fa-instagram"></i></a></li>
+            </ul>
+        </div>
+
       </div>
-
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -169,6 +174,7 @@ export default {
     font-size: 1rem;
     text-align: center;
     line-height: 1.8rem;
+    top: 300px;
     right: 0;
     left: 0;
     margin: auto;
@@ -250,11 +256,18 @@ export default {
     transition: .5s;
   }
   #abotor{
+    /* background-image:url("https://lh3.googleusercontent.com/_9XoqeAV2iEsxtEm4ofIrAdTMwYHJHemxW13Y29R9oEqxWPV2G0tWuDVNCSzT05_tE-vl0fQf9RKRHbMK6kvmEc_tXfx2s7syV-DQ5aqJ1hVmCxSrpKaZz-i4o8IN_GAQ0CiZ8ezw-c=w2400"); */
+    margin: 100px auto 0 auto;
+  }
+  .abotor{
+    position:absolute;
+    right: 0;
+    left: 0;
+    margin: auto;
+    border-radius: 50%;
     height: 210px;
     width: 210px;
-    border-radius: 50%;
-    background-color: white;
-    margin: 100px auto 0 auto;
+    object-fit: cover;
   }
   #menu{
     color: white;
