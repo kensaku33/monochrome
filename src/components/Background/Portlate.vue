21e6307e381f3e1bb6ca3a7470c1c58157cf6b86
Kensaku33 <picow.zoe33@gamil.com>
2020-02-09 13:34:37 +0900

    new page

---
+++ src/components/Background/Portlate.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="portlate">
+    <img class="image-portlate" src="https://lh3.googleusercontent.com/-QeTgMJifiytQUpUjdHuruLskAskK1dc7TKLbjkj1ida-zFEBw8F5_oYcJhVRKqmrUrVl10roLVuuaDUo8AYeuJzA7OKkJHsSOJHoMwjSnirqPIAh5tA1EtZCs60YvdNX8JongAEqeY=w2400" alt="">
+    
+  </div>
+</template>
+
+<style scoped>
+  #portlate{
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+  .image-portlate{
+    height: 100vh;
+    width: 100vw;
+    object-fit: cover;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import LandScape from '../components/Background/LandScape.vue'
+import Portlate from '../components/Background/Portlate.vue'
+import Snap from '../components/Background/Snap.vue'
 
 Vue.use(VueRouter)
 
@@ -17,7 +20,23 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/landscape',
+    name: 'landscape',
+    component: LandScape
+  },
+  {
+    path: '/portlate',
+    name: 'portlate',
+    component: Portlate
+  },
+  {
+    path: '/snap',
+    name: 'snap',
+    component: Snap
+  },
+
 ]
 
 const router = new VueRouter({
